241d4b29f6309adf2838cc879d3a92e0f3e23d09
Robert Wafle <Robert.Wafle@10thmagnitude.com>
2022-10-18 21:47:05 +0000

    put back accidentally commented lines

--- provider/common_cluster.go
+++ provider/common_cluster.go
@@ -15,56 +15,56 @@ func MapClusterFromSchema(d *schema.ResourceData) (*tc.Cluster, error) {
 	enableNamespaceExclusions := d.Get("enable_namespace_exclusions").(bool)
 	autoInstallServiceMesh := d.Get("auto_install_servicemesh").(bool)
 
-	// _tags := d.Get("tags").(*schema.Set).List()
-	// tags := make([]string, len(_tags))
+	_tags := d.Get("tags").(*schema.Set).List()
+	tags := make([]string, len(_tags))
 
-	// // workaround for unit test !?!?
-	// // reverse list order to pass unit test... this order probably doesn't really matter anyways..
-	// // deep compare doesn't have the ability to ignore hte order of the list
-	// for i, tag := range _tags {
-	// 	//fmt.Print(tag)
-	// 	//tags[i] = tag.(string)
-	// 	tags[len(_tags)-i-1] = tag.(string)
+	// workaround for unit test !?!?
+	// reverse list order to pass unit test... this order probably doesn't really matter anyways..
+	// deep compare doesn't have the ability to ignore hte order of the list
+	for i, tag := range _tags {
+		//fmt.Print(tag)
+		//tags[i] = tag.(string)
+		tags[len(_tags)-i-1] = tag.(string)
 
-	// }
-	// //copy(tags, _tags)
+	}
+	//copy(tags, _tags)
 
-	// _labels := d.Get("labels").(map[string]interface{})
-	// //labels := []tc.Label{}
-	// labels := make([]tc.Label, len(_labels))
+	_labels := d.Get("labels").(map[string]interface{})
+	//labels := []tc.Label{}
+	labels := make([]tc.Label, len(_labels))
 
-	// i := 0
-	// labelsLen := len(_labels)
-	// for key, value := range _labels {
-	// 	//fmt.Printf("\nkey:%s\n", key)
-	// 	//fmt.Printf("\nvalue:%s\n", value)
-	// 	m := make(map[string]interface{})
-	// 	m[key] = value
-	// 	label := tc.Label{
-	// 		Key:   key,
-	// 		Value: value.(string),
-	// 	}
-	// 	//labels[i] = label
-	// 	labels[labelsLen-i-1] = label
-	// 	i = i + 1
-	// 	//labels = append(labels, label)
-	// }
+	i := 0
+	labelsLen := len(_labels)
+	for key, value := range _labels {
+		//fmt.Printf("\nkey:%s\n", key)
+		//fmt.Printf("\nvalue:%s\n", value)
+		m := make(map[string]interface{})
+		m[key] = value
+		label := tc.Label{
+			Key:   key,
+			Value: value.(string),
+		}
+		//labels[i] = label
+		labels[labelsLen-i-1] = label
+		i = i + 1
+		//labels = append(labels, label)
+	}
 
-	// _namespace_exclusions := d.Get("namespace_exclusion").(*schema.Set).List()
-	// //namespace_exclusions := []tc.NamespaceExclusion{}
-	// namespace_exclusions := make([]tc.NamespaceExclusion, len(_namespace_exclusions))
+	_namespace_exclusions := d.Get("namespace_exclusion").(*schema.Set).List()
+	//namespace_exclusions := []tc.NamespaceExclusion{}
+	namespace_exclusions := make([]tc.NamespaceExclusion, len(_namespace_exclusions))
 
-	// for i, namespace_exclusion := range _namespace_exclusions {
-	// 	ne, lbok := namespace_exclusion.(map[string]any)
-	// 	if lbok {
-	// 		namespace_exclusion := tc.NamespaceExclusion{
-	// 			Match: ne["match"].(string),
-	// 			Type:  ne["type"].(string),
-	// 		}
-	// 		//namespace_exclusions = append(namespace_exclusions, namespace_exclusion)
-	// 		namespace_exclusions[len(_namespace_exclusions)-i-1] = namespace_exclusion
-	// 	}
-	// }
+	for i, namespace_exclusion := range _namespace_exclusions {
+		ne, lbok := namespace_exclusion.(map[string]any)
+		if lbok {
+			namespace_exclusion := tc.NamespaceExclusion{
+				Match: ne["match"].(string),
+				Type:  ne["type"].(string),
+			}
+			//namespace_exclusions = append(namespace_exclusions, namespace_exclusion)
+			namespace_exclusions[len(_namespace_exclusions)-i-1] = namespace_exclusion
+		}
+	}
 
 	cluster := tc.Cluster{
 		ID:                        ID,
@@ -73,9 +73,9 @@ func MapClusterFromSchema(d *schema.ResourceData) (*tc.Cluster, error) {
 		Token:                     token,
 		AutoInstallServiceMesh:    autoInstallServiceMesh,
 		EnableNamespaceExclusions: enableNamespaceExclusions,
-		//Tags:                      tags,
-		//NamespaceExclusions:       namespace_exclusions,
-		//Labels:                    labels,
+		Tags:                      tags,
+		NamespaceExclusions:       namespace_exclusions,
+		Labels:                    labels,
 	}
 
 	return &cluster, nil
