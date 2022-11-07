d246fed7763cd0492cfe5d0932f4c44d7b7377b8
Robert Wafle <robert.wafle@cantire.com>
2022-11-07 16:19:32 -0600

    update .devcontainer to possibly work on m1

---
+++ .devcontainer/fix-sources.sh
@@ -0,0 +1,24 @@
+mkdir ~/solution
+cd ~/solution/
+
+cat << EOF > ~/solution/sources.list
+deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
+
+deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+
+deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
+
+deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+
+deb http://archive.canonical.com/ubuntu focal partner
+deb-src http://archive.canonical.com/ubuntu focal partner
+EOF
+
+sudo rm /etc/apt/sources.list
+sudo cp ~/solution/sources.list /etc/apt/sources.list
+
+#sudo mv /etc/apt/sources.list.d/* ~/solution
